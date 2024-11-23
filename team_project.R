a57a121cb935f88b407a84dfb93f14613f750060
Trinley Palmo <t.palmo@hotmail.com>
2024-11-23 13:36:47 -0500

    Renamed half of columns

--- team_project.R
+++ team_project.R
@@ -1,2 +1,21 @@
 library(readxl)
+library(dplyr)
 data <- read_excel("transfusion data.xlsx")
+data <- data %>% 
+  rename(
+       study_id = "STUDY ID #",
+       gender_male = "Gender (male)",
+       aat_deficiency = "alpha1-Antitrypsin Deficiency",
+       cys_fib = "Cystic Fibrosis",
+       ipah = "Idiopathic Pulmonary Hypertension",
+       ild = "Interstitial Lung Disease",
+       pulm_other = "Pulm_Other",
+       cad = "Coronary Artery Disease",
+       t1d = "Diabetes (insulin)",
+       t2d = "Diabetes (diet/OHGs)",
+       gerd_pud = "GERD/PUD",
+       renal_fail = "Renal Failure",
+       stroke = "Stroke/CVA",
+       liver_disease = "Liver Disease",
+       thyroid_disease = "Thyroid Disease"
+       )
